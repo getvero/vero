5eaf646a714fe1772e50901d018c7bc7e793396c
Tu Hoang <rebyn@me.com>
2015-07-25 09:46:28 +1000

    Refactor api_spec & config_spec using `let`s.

--- spec/lib/api_spec.rb
+++ spec/lib/api_spec.rb
@@ -23,76 +23,58 @@ describe Vero::Api::Events do
 end
 
 describe Vero::Api::Users do
-  let (:subject) { Vero::Api::Users }
+  let(:subject) { Vero::Api::Users }
+  let(:mock_context) { Vero::Context.new }
+  let(:expected) { input.merge(:auth_token => "abc123", :development_mode => true) }
+
+  before :each do
+    allow(mock_context.config).to receive(:configured?).and_return(true)
+    allow(mock_context.config).to receive(:auth_token).and_return("abc123")
+    allow(mock_context.config).to receive(:development_mode).and_return(true)
+    allow(Vero::App).to receive(:default_context).and_return(mock_context)
+  end
 
   describe :track! do
-    it "should call the TrackAPI object via the configured sender" do
-      input = {:email => "james@getvero.com", :data => {:age => 25}}
-      expected = input.merge(:auth_token => "abc123", :development_mode => true)
+    context "should call the TrackAPI object via the configured sender" do
+      let(:input) { {:email => "james@getvero.com", :data => {:age => 25}} }
 
-      mock_context = Vero::Context.new
-      allow(mock_context.config).to receive(:configured?).and_return(true)
-      allow(mock_context.config).to receive(:auth_token).and_return("abc123")
-      allow(mock_context.config).to receive(:development_mode).and_return(true)
-
-      allow(Vero::App).to receive(:default_context).and_return(mock_context)
-
-      expect(Vero::Sender).to receive(:send).with(Vero::Api::Workers::Users::TrackAPI, true, "https://api.getvero.com", expected)
-
-      subject.track!(input)
+      specify do
+        expect(Vero::Sender).to receive(:send).with(Vero::Api::Workers::Users::TrackAPI, true, "https://api.getvero.com", expected)
+        subject.track!(input)
+      end
     end
   end
 
   describe :edit_user! do
-    it "should call the TrackAPI object via the configured sender" do
-      input = {:email => "james@getvero.com", :changes => {:age => 25}}
-      expected = input.merge(:auth_token => "abc123", :development_mode => true)
+    context "should call the TrackAPI object via the configured sender" do
+      let(:input) { {:email => "james@getvero.com", :changes => {:age => 25}} }
 
-      mock_context = Vero::Context.new
-      allow(mock_context.config).to receive(:configured?).and_return(true)
-      allow(mock_context.config).to receive(:auth_token).and_return("abc123")
-      allow(mock_context.config).to receive(:development_mode).and_return(true)
-
-      allow(Vero::App).to receive(:default_context).and_return(mock_context)
-
-      expect(Vero::Sender).to receive(:send).with(Vero::Api::Workers::Users::EditAPI, true, "https://api.getvero.com", expected)
-
-      subject.edit_user!(input)
+      specify do
+        expect(Vero::Sender).to receive(:send).with(Vero::Api::Workers::Users::EditAPI, true, "https://api.getvero.com", expected)
+        subject.edit_user!(input)
+      end
     end
   end
 
   describe :edit_user_tags! do
-    it "should call the TrackAPI object via the configured sender" do
-      input = {:add => ["boom"], :remove => ["tish"]}
-      expected = input.merge(:auth_token => "abc123", :development_mode => true)
+    context "should call the TrackAPI object via the configured sender" do
+      let(:input) { {:add => ["boom"], :remove => ["tish"]} }
 
-      mock_context = Vero::Context.new
-      allow(mock_context.config).to receive(:configured?).and_return(true)
-      allow(mock_context.config).to receive(:auth_token).and_return("abc123")
-      allow(mock_context.config).to receive(:development_mode).and_return(true)
-
-      allow(Vero::App).to receive(:default_context).and_return(mock_context)
-
-      expect(Vero::Sender).to receive(:send).with(Vero::Api::Workers::Users::EditTagsAPI, true, "https://api.getvero.com", expected)
-
-      subject.edit_user_tags!(input)
+      specify do
+        expect(Vero::Sender).to receive(:send).with(Vero::Api::Workers::Users::EditTagsAPI, true, "https://api.getvero.com", expected)
+        subject.edit_user_tags!(input)
+      end
     end
   end
 
   describe :unsubscribe! do
-    it "should call the TrackAPI object via the configured sender" do
-      input = {:email => "james@getvero"}
-      expected = input.merge(:auth_token => "abc123", :development_mode => false)
+    context "should call the TrackAPI object via the configured sender" do
+      let(:input) { {:email => "james@getvero"} }
 
-      mock_context = Vero::Context.new
-      allow(mock_context.config).to receive(:configured?).and_return(true)
-      allow(mock_context.config).to receive(:auth_token).and_return("abc123")
-
-      allow(Vero::App).to receive(:default_context).and_return(mock_context)
-
-      expect(Vero::Sender).to receive(:send).with(Vero::Api::Workers::Users::UnsubscribeAPI, true, "https://api.getvero.com", expected)
-
-      subject.unsubscribe!(input)
+      specify do
+        expect(Vero::Sender).to receive(:send).with(Vero::Api::Workers::Users::UnsubscribeAPI, true, "https://api.getvero.com", expected)
+        subject.unsubscribe!(input)
+      end
     end
   end
 end
--- spec/lib/config_spec.rb
+++ spec/lib/config_spec.rb
@@ -1,76 +1,74 @@
 require 'spec_helper'
 
 describe Vero::Config do
-  before :each do
-    @config = Vero::Config.new
-  end
+  let(:config) { Vero::Config.new }
 
   it "should be async by default" do
-    expect(@config.async).to be(true)
+    expect(config.async).to be(true)
   end
 
   describe :reset! do
     it "should reset all attributes" do
-      @config.api_key = "abcd1234"
-      @config.secret  = "abcd1234"
+      config.api_key = "abcd1234"
+      config.secret  = "abcd1234"
+      config.reset!
 
-      @config.reset!
-      expect(@config.api_key).to be_nil
-      expect(@config.secret).to be_nil
+      expect(config.api_key).to be_nil
+      expect(config.secret).to be_nil
     end
   end
 
   describe :auth_token do
     it "should return nil if either api_key or secret are not set" do
-      @config.api_key = nil
-      @config.secret = "abcd"
-      expect(@config.auth_token).to be_nil
+      config.api_key = nil
+      config.secret = "abcd"
+      expect(config.auth_token).to be_nil
 
-      @config.api_key = "abcd"
-      @config.secret = nil
-      expect(@config.auth_token).to be_nil
+      config.api_key = "abcd"
+      config.secret = nil
+      expect(config.auth_token).to be_nil
 
-      @config.api_key = "abcd"
-      @config.secret = "abcd"
-      expect(@config.auth_token).not_to be_nil
+      config.api_key = "abcd"
+      config.secret = "abcd"
+      expect(config.auth_token).not_to be_nil
     end
 
     it "should return an expected auth_token" do
-      @config.api_key = "abcd1234"
-      @config.secret = "efgh5678"
-      expect(@config.auth_token).to eq("YWJjZDEyMzQ6ZWZnaDU2Nzg=")
+      config.api_key = "abcd1234"
+      config.secret = "efgh5678"
+      expect(config.auth_token).to eq("YWJjZDEyMzQ6ZWZnaDU2Nzg=")
     end
   end
 
   describe :request_params do
     it "should return a hash of auth_token and development_mode if they are set" do
-      @config.api_key = nil
-      @config.secret = nil
-      @config.development_mode = nil
-      expect(@config.request_params).to eq({})
+      config.api_key = nil
+      config.secret = nil
+      config.development_mode = nil
+      expect(config.request_params).to eq({})
 
-      @config.api_key = "abcd1234"
-      @config.secret = "abcd1234"
-      expect(@config.request_params).to eq({:auth_token => "YWJjZDEyMzQ6YWJjZDEyMzQ="})
+      config.api_key = "abcd1234"
+      config.secret = "abcd1234"
+      expect(config.request_params).to eq({:auth_token => "YWJjZDEyMzQ6YWJjZDEyMzQ="})
 
-      @config.development_mode = true
-      expect(@config.request_params).to eq({:auth_token => "YWJjZDEyMzQ6YWJjZDEyMzQ=", :development_mode => true})
+      config.development_mode = true
+      expect(config.request_params).to eq({:auth_token => "YWJjZDEyMzQ6YWJjZDEyMzQ=", :development_mode => true})
     end
   end
 
   describe :domain do
     it "should return https://api.getvero.com when not set" do
-      expect(@config.domain).to eq('https://api.getvero.com')
-      @config.domain = 'blah.com'
-      expect(@config.domain).not_to eq('https://api.getvero.com')
+      expect(config.domain).to eq('https://api.getvero.com')
+      config.domain = 'blah.com'
+      expect(config.domain).not_to eq('https://api.getvero.com')
     end
 
     it "should return the domain value" do
-      @config.domain = 'test.unbelieveable.com.au'
-      expect(@config.domain).to eq('http://test.unbelieveable.com.au')
+      config.domain = 'test.unbelieveable.com.au'
+      expect(config.domain).to eq('http://test.unbelieveable.com.au')
 
-      @config.domain = 'http://test.unbelieveable.com.au'
-      expect(@config.domain).to eq('http://test.unbelieveable.com.au')
+      config.domain = 'http://test.unbelieveable.com.au'
+      expect(config.domain).to eq('http://test.unbelieveable.com.au')
     end
   end
 
@@ -93,11 +91,11 @@ describe Vero::Config do
     end
 
     it "can be overritten with the config block" do
-      @config.development_mode = true
-      expect(@config.request_params[:development_mode]).to be(true)
+      config.development_mode = true
+      expect(config.request_params[:development_mode]).to be(true)
 
-      @config.reset!
-      expect(@config.request_params[:development_mode]).to be(false)
+      config.reset!
+      expect(config.request_params[:development_mode]).to be(false)
     end
   end
 end
