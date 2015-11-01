a7037f59c4b46d458ed6909ac468f789e76ae444
James Lamont <james@semblancesystems.com>
2015-11-02 09:12:34 +1100

    Minor refactor.

--- lib/vero/senders/sidekiq.rb
+++ lib/vero/senders/sidekiq.rb
@@ -6,17 +6,31 @@ module Vero
     include ::Sidekiq::Worker
 
     def perform(api_class, domain, options)
-      api_class.constantize.new(domain, options).perform
-      Vero::App.log(self, "method: #{api_class}, options: #{options.to_json}, response: sidekiq job queued")
+      send_to_vero(api_class, domain, options)
+    end
+
+    protected
+
+    def send_to_vero(api_class, domain, options)
+      api_klass(api_class).new(domain, options).perform
+      Vero::App.log(self, "method: #{api_class}, options: #{options.to_json}, response: sidekiq job performed")
+    end
+
+    def api_klass(api_class)
+      api_class.constantize
     end
   end
 
   module Senders
     class Sidekiq
       def call(api_class, domain, options)
-        response = ::Vero::SidekiqWorker.perform_async(api_class.to_s, domain, options)
+        response = sender_class.perform_async(api_class.to_s, domain, options)
         Vero::App.log(self, "method: #{api_class.name}, options: #{options.to_json}, response: sidekiq job queued")
         response
+      end
+
+      def sender_class
+        ::Vero::SidekiqWorker
       end
     end
   end
