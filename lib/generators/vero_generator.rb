00275c01fe5e22c7bca8e5622c2e51cec0deb2ce
James Lamont <958588+jylamont@users.noreply.github.com>
2025-02-19 16:27:12 -0500

    bug: Fixed readme and generator (#87)
    Changes made by https://github.com/getvero/vero/pull/80 weren't reflected in the README or Rails Generator. This PR updates both files with correct instructions.

--- lib/generators/vero_generator.rb
+++ lib/generators/vero_generator.rb
@@ -1,34 +1,11 @@
 # frozen_string_literal: true
 
 class VeroGenerator < Rails::Generators::Base
-  class_option :heroku
-  class_option :api_key
-  class_option :api_secret
-
   def create_initializer_file
-    type = options[:heroku] || "standard"
-
-    abort("You must provide an API KEY and API SECRET to proceed.") if options[:heroku].blank? && (options[:api_key].blank? || options[:api_secret].blank?)
-    create_file "config/initializers/vero.rb", send(:"#{type}_initializer_content")
-  end
-
-  private
-
-  def standard_initializer_content
-    <<~END_TEXT
+    create_file("config/initializers/vero.rb", <<~INITIALIZER)
       Vero::App.init do |config|
-        config.api_key  = '#{options[:api_key]}'
-        config.secret   = '#{options[:api_secret]}'
+        config.tracking_api_key = ENV['VERO_TRACKING_API_KEY']
       end
-    END_TEXT
-  end
-
-  def heroku_initializer_content
-    <<~END_TEXT
-      Vero::App.init do |config|
-        config.api_key  = ENV['VERO_API_KEY']
-        config.secret   = ENV['VERO_API_SECRET']
-      end
-    END_TEXT
+    INITIALIZER
   end
 end
